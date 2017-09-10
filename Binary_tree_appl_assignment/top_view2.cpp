2e3aabc7b2ca7d85a04d611567433f8d90290348
Samarjit Karmakar <karmakar.samarjit@gmail.com>
2017-09-10 19:28:44 +0530

    Add files via upload

---
+++ Binary_tree_appl_assignment/top_view2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+struct btnode
+{
+  btnode* left;
+  int data;
+  btnode* right;
+};
+
+struct node_vec
+{
+  int level;
+  int data;
+};
+
+typedef btnode* btptr;
+
+btptr createBTNode(int n)
+{
+  btptr newNode=new btnode;
+  newNode->data=n; newNode->left=NULL; newNode->right=NULL;
+  return newNode;
+}
+
+void create_init_tree(btptr &root)
+{
+    cout << "Enter data:"; int n; cin >> n;
+    root=createBTNode(n);
+    cout << "Does " << n << " have a left child:"; char ans; cin >> ans;
+    if(ans=='y')create_init_tree(root->left);
+    cout << "Does " << n << " have a right child:"; cin >> ans;
+    if(ans=='y')create_init_tree(root->right);
+}
+
+vector<node_vec> a;
+void printTopLeft(btptr root, int level=0)
+{
+  if(root==NULL)return;
+  node_vec curr;
+  curr.data=root->data; curr.level=level; int flag=0;
+  for(int i=0; i<a.size(); i++)
+  {
+    if(a[i].level==level){ flag=1; break;}
+  }
+  if(flag==0)a.push_back(curr);
+
+  printTopLeft(root->left, level-1);
+  printTopLeft(root->right, level+1);
+}
+
+bool cmp(const node_vec a, const node_vec b)
+{
+  return a.level<b.level;
+}
+
+void printTopView(btptr root)
+{
+  printTopLeft(root);
+
+  sort(a.begin(), a.end(), cmp);
+
+  for(int i=0; i<a.size(); i++)cout << a[i].data << " ";
+  cout << endl;
+}
+
+int main()
+{
+  btptr head=NULL;
+  create_init_tree(head);
+  cout << endl;
+  //vector<int> v;
+  printTopView(head);
+  //for(int i=0; i<v.size(); i++)cout << v[i] << " ";
+  return 0;
+}
--- Binary_tree_appl_assignment/tree_as_it_is_2.cpp
+++ Binary_tree_appl_assignment/tree_as_it_is_2.cpp
@@ -27,6 +27,48 @@ void create_init_tree(btptr &root)
     cout << "Does " << n << " have a right child:"; cin >> ans;
     if(ans=='y')create_init_tree(root->right);
 }
+/*
+vector<int> a;
+void order_store(btptr root)
+{
+  if(root==NULL)return;
+  else
+  {
+    order_store(root->left);
+    a.push_back(root->data);
+    order_store(root->right);
+  }
+}
+*/
+/*
+void levelorder(btptr root)
+{
+  if(root==NULL)return;
+
+  btptr spcl=new btnode;
+  spcl->data=-1; spcl->left=NULL; spcl->right=NULL;
+
+  queue<btptr> q;
+  q.push(root); int j=0, k=0;
+  while(!q.empty())
+  {
+    int i=q.size();
+
+    while(i>0)
+    {
+      btptr tmp=q.front(); q.pop();
+      int sp;
+      for(sp=0; sp<a.size(); sp++){if(tmp->data==a[sp])break;}
+      while(j<sp){j++; k++; cout << " ";}
+      cout << tmp->data;
+      if(tmp->left!=NULL)q.push(tmp->left);
+      if(tmp->right!=NULL)q.push(tmp->right);
+      i--;
+    }
+    cout << endl; j=0;
+  }
+}
+*/
 
 void printAsItIs(btptr root, int sp=0)
 {

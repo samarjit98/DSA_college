9f091aa4a6bd14abd6578ac34a0af29663cfbf10
Samarjit Karmakar <karmakar.samarjit@gmail.com>
2017-09-10 19:24:15 +0530

    Update create_binary_tree_one_line.cpp

--- Binary_tree_appl_assignment/create_binary_tree_one_line.cpp
+++ Binary_tree_appl_assignment/create_binary_tree_one_line.cpp
@@ -16,17 +16,7 @@ btptr createBTNode(char n)
   newNode->data=n; newNode->left=NULL; newNode->right=NULL;
   return newNode;
 }
-/*
-void create_init_tree(btptr &root)
-{
-    cout << "Enter data:"; int n; cin >> n;
-    root=createBTNode(n);
-    cout << "Does " << n << " have a left child:"; char ans; cin >> ans;
-    if(ans=='y')create_init_tree(root->left);
-    cout << "Does " << n << " have a right child:"; cin >> ans;
-    if(ans=='y')create_init_tree(root->right);
-}
-*/
+
 void preorder(btptr root)
 {
   if(root!=NULL)
@@ -36,21 +26,7 @@ void preorder(btptr root)
     preorder(root->right);
   }
 }
-/*
-void create_tree_using_string(btptr &root, string s, int i)
-{
-  if(i>=s.size())return;
 
-  root=createBTNode(s[i]);
-  i++;
-  if(s[i]!='.' && s[i+1]=='.')create_tree_using_string(root->left, s, i);
-  else if(s[i]=='.' && s[i+1]!='.')create_tree_using_string(root->right, s, i+1);
-  else if(s[i]!='.' && s[i+1]!='.')
-  {
-    create_tree_using_string(root->left, s, i); create_tree_using_string(root->right, s, i+1);
-  }
-}
-*/
 void create_tree_using_string(btptr &root, string s)
 {
   static int i=0;
@@ -60,21 +36,6 @@ void create_tree_using_string(btptr &root, string s)
   if(s[i]!='.'){ create_tree_using_string(root->left, s);}
   if(s[i]=='.'){ i++;}
   if(s[i]!='.'){ create_tree_using_string(root->right, s);}
-  /*
-  int j=i+1, vars=0, dots=0;
-  while(vars-1!=dots && j<s.size())
-  {
-    if(s[i]!='.')
-    {
-      vars++; j++;
-    }
-    else if(s[i]=='.' && s[i-1]!='.' && j>i+1)
-    {
-      dots++; j++;
-    }
-  }
-  if(j<s.size() && s[j]!='.')create_tree_using_string(root->right, s, j);
-  */
 }
 
 int main()

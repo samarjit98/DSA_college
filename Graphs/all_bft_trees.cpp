6019c1e80d3a60ddddf8a7de7dfebe81a4110e42
Samarjit Karmakar <karmakar.samarjit@gmail.com>
2017-10-06 01:51:47 +0530

    Update all_bft_trees.cpp

--- Graphs/all_bft_trees.cpp
+++ Graphs/all_bft_trees.cpp
@@ -65,9 +65,9 @@ public:
     vector<bool> visited(n, false);
     vector<int> path;
     /*
-    There is a very fundamental and minute difference between using a vector and and array
+    There is a very fundamental and minute difference between using a vector instead of an array.
     Hint: It lies in the passing mechanisms :P xD!!
-    This small difference makes it from normal DFS to AllDFS!
+    This small difference makes a normal DFS, an extremely awesome AllDFS!
     */
     allDFSUtil(s, visited, path);
   }
